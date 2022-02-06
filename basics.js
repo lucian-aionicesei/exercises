e2a4373a350f4e3bf5e940f9066fc42a56942fb0
Lucian Aionicesei <lucian.aionicesei@gmail.com>
2022-02-06 18:21:35 +0100

    publishing main folder

--- basics.js
+++ basics.js
@@ -1,1 +1,33 @@
+"use strict";
+// let y = 14;
+// let c = 3;
+
 console.log("Welcome!")
+
+const bool = true;
+const num = 41;
+const str = "Lucian";
+const obj = {
+    cats: 2,
+    cars: 1
+};
+const nothing = null;
+let undf;
+// const symbol = Symbol("Symbol");
+
+console.log(obj);
+console.log(typeof obj);
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+const value = " ";
+
+if (value) {
+    console.log("Valuse is truthy");
+} else {
+    console.log("Value is falsy");
+}
